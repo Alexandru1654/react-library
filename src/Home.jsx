f420f61c495920c21afee1f0c48ab30a4e025d51
alexandru233847 <alexutualexino33@gmail.com>
2024-11-25 11:42:53 +0100

    Fix JSX syntax

---
+++ src/Home.jsx
@@ -0,0 +1,26 @@
+export default function Home() {
+    return (
+        <div>
+            <header>
+                <div className="logo">
+                    LOGO
+                </div>
+                <nav>
+                    <ul>
+                        <a href="/">Home</a>
+                        <a href="/books">Books</a>
+                        <a href="/about">About</a>
+                        <a href="/contact">Contact</a>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <h1>About Page</h1>
+            </main>
+
+            <footer>
+            <p>copyright 2024</p>
+            </footer>
+        </div>
+    );
+}
