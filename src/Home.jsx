7575299f88d273a43ee34c041601e61d20c86069
alexandru233847 <alexutualexino33@gmail.com>
2024-11-25 12:36:56 +0100

    Export default function

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,22 @@
+import MainMenu from "../../MainMenu";
+
 export default function Home() {
     return (
-        <div>
-            <header>
-                <div className="logo">
-                    LOGO
-                </div>
-                <nav>
-                    <ul>
-                        <a href="/">Home</a>
-                        <a href="/books">Books</a>
-                        <a href="/about">About</a>
-                        <a href="/contact">Contact</a>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <h1>About Page</h1>
-            </main>
+        <> 
+        <header>
+            <div className="logo">
+                LOGO
+            </div>
+            <MainMenu />
 
-            <footer>
+        </header>
+        <main>
+            <h1>Books Page</h1>
+        </main>
+
+        <footer>
             <p>copyright 2024</p>
-            </footer>
-        </div>
-    );
+        </footer>
+        </>
+    )
 }
-
